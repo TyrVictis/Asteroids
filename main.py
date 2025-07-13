986adaf3405125c3302824c1e8feb0acae52ad68
TyrVictis <github.flask803@passmail.net>
2025-07-12 23:29:00 -0400

    Add circleshape.py and player.py, update main.py and constraints.py.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print ("Starting Asteroids!")
@@ -9,15 +11,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick() / 1000
-
+        dt = clock.tick(60) / 1000
+    
 
 if __name__ == "__main__":
     main()
