57750c54551be5f89a3564015fbad4f04cfc631e
TyrVictis <github.flask803@passmail.net>
2025-07-18 21:08:11 -0400

    Add asteroid.py and asteroidfield.py

--- asteroid.py
+++ asteroid.py
@@ -5,6 +5,6 @@ class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
     def draw(self, screen):
-        pygame.drase.circle(screen, "white", self.position, self.radius, 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
     def update(self, dt):
         self.position += self.velocity * dt
