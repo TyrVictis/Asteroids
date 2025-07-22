1ce6432887d1b1eda387d51cfb842599b12b7a88
TyrVictis <github.flask803@passmail.net>
2025-07-21 20:28:33 -0400

    Fixed typo in main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def main():
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init
+    pygame.init()
     clock = pygame.time.Clock()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
-    Asteroid.containters = (asteroids, updatable, drawable)
-    AsteroidField.containers = updatable
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     asteroid_field = AsteroidField()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
